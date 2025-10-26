fb9d460b755a919ea116f4b650a94b4cd04b00da
Rayyan Hussain <67929557+rayyanh192@users.noreply.github.com>
2025-10-26 07:36:38 -0700

    Fix typo in scrollTo method call

--- src/pages/IndexHome.vue
+++ src/pages/IndexHome.vue
@@ -45,7 +45,7 @@
               outlined
               height="60px"
               width="230px"
-              @click="scrolTo(link.to)"
+              @click="scrollTo(link.to)"
             >
               {{ link.title }}
             </v-btn>
